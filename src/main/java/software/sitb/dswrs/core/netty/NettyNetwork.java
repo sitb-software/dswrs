4c74e1a38d134dde40ab76370b9a84e354846937
Sean <sean.snow@live.com>
2016-01-06 12:34:29 +0800

    add addHandler

--- src/main/java/software/sitb/dswrs/core/netty/NettyNetwork.java
+++ src/main/java/software/sitb/dswrs/core/netty/NettyNetwork.java
@@ -1,6 +1,11 @@
 package software.sitb.dswrs.core.netty;
 
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelPipeline;
+
 /**
+ * netty网络服务接口定义
+ *
  * @author Sean sean.snow@live.com
  */
 public interface NettyNetwork<I, O> {
@@ -12,4 +17,18 @@ public interface NettyNetwork<I, O> {
     Class<I> getRequestClazz();
 
     Class<O> getResponseClazz();
+
+    /**
+     * 添加额外的消息编码器
+     *
+     * @param pipeline 管道
+     */
+    void addHandler(ChannelPipeline pipeline);
+
+    /**
+     * 获取一个消息处理器
+     *
+     * @return 消息处理器
+     */
+    ChannelHandler getMessageHandler();
 }
