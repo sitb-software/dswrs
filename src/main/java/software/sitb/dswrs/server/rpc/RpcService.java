b190d9a29f60f22af6416d76c209a7da30cdecd8
Sean <sean.snow@live.com>
2016-07-12 19:43:16 +0800

    删除Spring 的注解继承

--- src/main/java/software/sitb/dswrs/server/rpc/RpcService.java
+++ src/main/java/software/sitb/dswrs/server/rpc/RpcService.java
@@ -1,23 +1,15 @@
 package software.sitb.dswrs.server.rpc;
 
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-
 import java.lang.annotation.*;
 
 /**
  * RPC 标注该服务为RPC服务
  *
  * @author Sean sean.snow@live.com
- * @see Component
- * @see Repository
- * @see Service
  */
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Service
 public @interface RpcService {
     Class<?> value();
 }
