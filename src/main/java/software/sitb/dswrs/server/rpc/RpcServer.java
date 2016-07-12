c31655c7d9990d0ecb33d47d4c663f7690fe8ff7
Sean <sean.snow@live.com>
2016-07-12 19:41:06 +0800

    初始化保存RPC Bean 动态添加Bean 手动启动RPC服务

--- src/main/java/software/sitb/dswrs/server/rpc/RpcServer.java
+++ src/main/java/software/sitb/dswrs/server/rpc/RpcServer.java
@@ -1,6 +1,9 @@
 package software.sitb.dswrs.server.rpc;
 
 import io.netty.channel.ChannelPipeline;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
 import software.sitb.dswrs.core.ServiceRegistry;
 import software.sitb.dswrs.core.config.DswrsProperties;
@@ -9,6 +12,8 @@ import software.sitb.dswrs.core.rpc.RpcRequest;
 import software.sitb.dswrs.core.rpc.RpcResponse;
 import software.sitb.dswrs.core.zookeeper.ZooKeeperConfig;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,7 +22,9 @@ import java.util.Map;
  *
  * @author Sean sean.snow@live.com
  */
-public class RpcServer extends ObjectServer<RpcRequest, RpcResponse> {
+public class RpcServer extends ObjectServer<RpcRequest, RpcResponse> implements InitializingBean {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RpcServer.class);
 
     private DswrsProperties properties;
 
@@ -29,7 +36,6 @@ public class RpcServer extends ObjectServer<RpcRequest, RpcResponse> {
 
     public RpcServer(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
-        this.init();
     }
 
     @Override
@@ -64,19 +70,29 @@ public class RpcServer extends ObjectServer<RpcRequest, RpcResponse> {
         pipeline.addLast(handler);
     }
 
-    protected void init() {
+    @Override
+    public void afterPropertiesSet() throws Exception {
         this.properties = applicationContext.getBean(DswrsProperties.class);
         this.serviceRegistry = applicationContext.getBean(ServiceRegistry.class);
         Map<String, Object> rpcBeans = applicationContext.getBeansWithAnnotation(RpcService.class);
         if (null != rpcBeans && !rpcBeans.isEmpty()) {
             this.rpcBeans = new HashMap<>(rpcBeans.size());
             rpcBeans.forEach((key, value) -> {
-                String interfaceName = value.getClass().getAnnotation(RpcService.class).value().getName();
-                this.rpcBeans.put(interfaceName, value);
+                Class<?> clazz = value.getClass();
+                try {
+                    Method method = clazz.getMethod("getTargetClass");
+                    Class<?> result = (Class<?>) method.invoke(value);
+                    String interfaceName = result.getAnnotation(RpcService.class).value().getName();
+                    addRpcBean(interfaceName, value);
+                    LOGGER.debug("RPC Bean [{}]", interfaceName);
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+                    String interfaceName = clazz.getAnnotation(RpcService.class).value().getName();
+                    addRpcBean(interfaceName, value);
+                    LOGGER.debug("RPC Bean [{}]", interfaceName);
+                }
             });
         }
     }
-
 
     /**
      * 添加一个RPC服务Bean
@@ -84,14 +100,19 @@ public class RpcServer extends ObjectServer<RpcRequest, RpcResponse> {
      * @param clazz 接口Class
      * @param bean  接口实现示例
      */
-    public void addRpcBeans(Class<?> clazz, Object bean) {
+    public void addRpcBean(Class<?> clazz, Object bean) {
         if (null != clazz && null != bean) {
             String interfaceName = clazz.getName();
-            this.rpcBeans.put(interfaceName, bean);
+            addRpcBean(interfaceName, bean);
         }
     }
 
+    private void addRpcBean(String beanName, Object bean) {
+        this.rpcBeans.put(beanName, bean);
+    }
+
     public void startRpcServer() throws InterruptedException {
+        LOGGER.debug("start rpc server.");
         this.start(() -> {
             ZooKeeperConfig config = this.properties.getZooKeeper();
             if (config.getEnabled()) {
@@ -100,5 +121,6 @@ public class RpcServer extends ObjectServer<RpcRequest, RpcResponse> {
             }
         });
     }
+
 
 }
